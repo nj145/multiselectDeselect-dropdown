46cddc850a25d74ce557e51c8b0e0581d196c6df
nj145 <122491358+nj145@users.noreply.github.com>
2023-03-10 17:09:11 -0600

    Updated the input list

--- src/Selection.js
+++ src/Selection.js
@@ -3,16 +3,36 @@ import { Grid, TextField, Typography } from "@mui/material";
 import MultiSelectAll from "./MultiSelectAll";
 
 const Selection = () => {
-  const initialValue = [{ label: "Uber", value: "5" }];
+  const initialValue = [{ label: "Uber/Sales", value: "14" }];
 
-  const companyNames = [
-    { label: "Amazon", value: "1" },
-    { label: "Google", value: "2" },
-    { label: "Netflix", value: "3" },
-    { label: "Microsoft", value: "4" },
-    { label: "Uber", value: "5" },
-    { label: "Walmart", value: "6" },
-    { label: "CVS", value: "7" }
+  const departmentNames = [
+    { label: "Amazon/Sales", value: "1" },
+    { label: "Amazon/Customer Support", value: "2" },
+    { label: "Google/Operations", value: "3" },
+    { label: "Google/Engineering", value: "4" },
+    { label: "Google/Services", value: "5" },
+    { label: "Google/Customer Support", value: "6" },
+    { label: "Netflix/Sales", value: "7" },
+    { label: "Netflix/Engineering", value: "8" },
+    { label: "Netflix/Services", value: "9" },
+    { label: "Netflix/Operations", value: "10" },
+    { label: "Microsoft/Sales", value: "11" },
+    { label: "Microsoft/Operations", value: "12" },
+    { label: "Microsoft/Customer Support", value: "13" },
+    { label: "Uber/Sales", value: "14" },
+    { label: "Uber/Services", value: "15" },
+    { label: "Uber/Customer Support", value: "16" },
+    { label: "Uber/Engineering", value: "17" },
+    { label: "Uber/Operations", value: "18" },
+    { label: "Walmart/Sales", value: "19" },
+    { label: "Walmart/Services", value: "20" },
+    { label: "Walmart/Operations", value: "21" },
+    { label: "Walmart/Engineering", value: "22" },
+    { label: "Walmart/Customer Support", value: "23" },
+    { label: "CVS/Sales", value: "24" },
+    { label: "CVS/Customer Support", value: "25" },
+    { label: "CVS/Engineering", value: "26" },
+    { label: "CVS/Operations", value: "27" }
   ];
 
   const getTextBoxInputValue = (input) => {
@@ -29,18 +49,19 @@ const Selection = () => {
   };
 
   return (
-    <Grid container>
-      <Grid item sx={{ p: 2 }}>
+    <Grid container flexDirection="column" alignItems="center">
+      <Grid item xs={12} sx={{ p: 2 }}>
         <MultiSelectAll
-          items={companyNames}
+          sx={{ maxheight: "700px" }}
+          items={departmentNames}
           selectAllLabel="Select All"
           value={initialValue}
           onChange={handleSelectionChange}
         />
       </Grid>
-      <Grid item sx={{ p: 2 }}>
+      <Grid item xs={12} sx={{ p: 2 }}>
         <Typography>Selected items:</Typography>
-        <TextField sx={{ width: "350px" }} value={currentSelection} />
+        <TextField sx={{ width: "450px" }} value={currentSelection} />
       </Grid>
     </Grid>
   );
